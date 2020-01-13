9923090c7ccb3071d8c916c48a6805007cc850c0
axmonsalve <axel.monsalve.ponce@gmail.com>
2020-01-13 17:35:17 -0300

    first commit

---
+++ src/components/VideoItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Grid, Typography, Paper } from '@material-ui/core';
+
+const VideoItem = () => {
+    return (
+        <h1>Video Item</h1>
+    )
+}
+ export default VideoItem;
---
+++ src/components/VideoList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+
+const VideoList = () => {
+    return(
+        <h1>Video list component</h1>
+    )
+}
+
+export default VideoList;
